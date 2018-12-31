dd9e67358299165b3e3b3c1286b51b65dc8820da
David zhang <d.ydzdavid@yahoo.com>
2018-12-31 21:04:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
+var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
+var i = 0;
 function dwarfRollCall(dwarves) {
+  var countingDwarves = [];
+  for (i = 0; i < dwarves.length; i++) {
+    countingDwarves.push([i+1] +". " + dwarves[i] + " ");
+  }
+  return countingDwarves.join("");
 }
-
+var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 function summonCaptainPlanet(planeteerCalls){
+  var capCall = [];
+  for (var i = 0; i < planeteerCalls.length; i++){
+    capCall.push(`${planeteerCalls[i]}`.toUpperCase() + "!")
+  }
+  return capCall;
 }
 
 function longPlaneteerCalls(words) {
+  if (words.length > 4) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function findTheCheese (foods) {
+    for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === 'cheddar') {
+      return foods[i]
+    }
+    else if (foods[i] === 'gouda') {
+      return foods[i]
+    }
+    else if (foods[i] === 'camembert') {
+      return foods[i]
+    }
+  }
+  return ('no cheese!');
 }
